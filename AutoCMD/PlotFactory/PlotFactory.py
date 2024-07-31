8bf427ec42fa388e3683370bf104ae3d2f029bfd
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-07-31 16:07:10 -0500

    init for Results class

--- AutoCMD/PlotFactory/PlotFactory.py
+++ AutoCMD/PlotFactory/PlotFactory.py
@@ -39,3 +39,68 @@ class Results:
 # this class will hold all the plotting functions, where
 # each function will make one figure, and a following function will join them
 class Plots:
+    @staticmethod
+    def unique_base_sequences(dataframe1: pd.DataFrame, dataframe2: pd.DataFrame) -> plt.Figure:
+        number_of_distinct_base_sequences_df1 = dataframe1["Base Sequence"].nunique()
+        number_of_distinct_base_sequences_df2 = dataframe2["Base Sequence"].nunique()
+
+        fig, ax = plt.subplots()
+
+        ax.bar(1, number_of_distinct_base_sequences_df1, width = 1, linewidth = 0.7, label="SSRCalc3")
+        ax.bar(4, number_of_distinct_base_sequences_df2, width = 1, linewidth = 0.7, label="Chronologer")
+
+        ax.set(xlim=(0, 8), xticks=np.arange(1, 6))
+
+        #removes the X Axis ticks
+        plt.xticks([])
+
+        ax.set_title("Unique Base Sequences in All Peptides File")
+
+        #dataframe1 annotation
+        ax.annotate("n = "+ ("{:,}").format(number_of_distinct_base_sequences_df1),
+                    xy=(1.3, number_of_distinct_base_sequences_df1-100000),
+                    xytext=(2, number_of_distinct_base_sequences_df1),
+                    arrowprops=dict(facecolor='red', width=2, headwidth=12))
+        
+        #dataframe two annotation
+        ax.annotate("n = "+ ("{:,}").format(number_of_distinct_base_sequences_df2),
+                    xy=(4.3, number_of_distinct_base_sequences_df2-100000),
+                    xytext=(6, number_of_distinct_base_sequences_df2),
+                    arrowprops=dict(facecolor='red', width=2, headwidth=12))
+
+        ax.legend()
+
+        return fig
+    
+    @staticmethod
+    def unique_full_sequences(dataframe1: pd.DataFrame, dataframe2: pd.DataFrame) -> plt.Figure:
+        number_of_distinct_base_sequences_df1 = dataframe1["Full Sequence"].nunique()
+        number_of_distinct_base_sequences_df2 = dataframe2["Full Sequence"].nunique()
+
+        fig, ax = plt.subplots()
+
+        ax.bar(1, number_of_distinct_base_sequences_df1, width = 1, linewidth = 0.7, label="SSRCalc3")
+        ax.bar(4, number_of_distinct_base_sequences_df2, width = 1, linewidth = 0.7, label="Chronologer")
+
+        ax.set(xlim=(0, 8), xticks=np.arange(1, 6))
+
+        #removes the X Axis ticks
+        plt.xticks([])
+
+        ax.set_title("Unique Full Sequences in All Peptides File")
+
+        #dataframe1 annotation
+        ax.annotate("n = "+ ("{:,}").format(number_of_distinct_base_sequences_df1),
+                    xy=(1.3, number_of_distinct_base_sequences_df1-100000),
+                    xytext=(2, number_of_distinct_base_sequences_df1),
+                    arrowprops=dict(facecolor='red', width=2, headwidth=12))
+        
+        #dataframe two annotation
+        ax.annotate("n = "+ ("{:,}").format(number_of_distinct_base_sequences_df2),
+                    xy=(4.3, number_of_distinct_base_sequences_df2-100000),
+                    xytext=(6, number_of_distinct_base_sequences_df2),
+                    arrowprops=dict(facecolor='red', width=2, headwidth=12))
+
+        ax.legend()
+
+        return fig
---
+++ AutoCMD/Plots/Plot.cs
@@ -0,0 +1,47 @@
+﻿using Proteomics.PSM;
+using Readers;
+using Readers.QuantificationResults;
+
+namespace Plots;
+
+public class Plot
+{
+
+}
+
+public class Results
+{
+    public string ResultsPath { get; set; }
+    public List<QuantifiedPeak> AllQuantifiedPeaks { get; set; }
+    public List<PsmFromTsv> AllPsms { get; set; }
+    public List<PsmFromTsv> AllPeptides { get; set; }
+    public Dictionary<string, (PsmFromTsvFile Peptides, PsmFromTsvFile PSMs, PsmFromTsvFile QuantifiedPeaks)> IndividualFileResults { get; set; }
+
+    public Results(string searchTaskPath)
+    {
+        ResultsPath = searchTaskPath;
+
+        var allQuantifiedPeaksFile = new QuantifiedPeakFile(Path.Join(searchTaskPath, "AllQuantifiesPeaks.tsv"));
+        allQuantifiedPeaksFile.LoadResults();
+        AllQuantifiedPeaks = allQuantifiedPeaksFile.Results;
+        
+        var allPsmsFile = new PsmFromTsvFile(Path.Join(searchTaskPath, "AllPSMs.psmtsv"));
+        allPsmsFile.LoadResults();
+        AllPsms = allPsmsFile.Results;
+
+        var allPeptidesFile = new PsmFromTsvFile(Path.Join(searchTaskPath, "AllPeptides.psmtsv"));
+        allPeptidesFile.LoadResults();
+        AllPeptides = allPeptidesFile.Results;
+
+        var filesUsed = AllPsms.Select(x => x.FileNameWithoutExtension)
+            .Distinct()
+            .ToList();
+
+        var filesInIndividualResults = Directory.GetFiles(Path.Join(searchTaskPath, "Individual File Results"));
+
+        foreach (var file in filesUsed)
+        {
+
+        }
+    }
+}
